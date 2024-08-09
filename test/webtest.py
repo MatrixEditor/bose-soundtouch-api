dd800348869f11dee6eeb8de923be08046585033
MatrixEditor <58256046+MatrixEditor@users.noreply.github.com>
2024-08-09 20:32:45 +0200

    Update simple webtest

--- test/webtest.py
+++ test/webtest.py
@@ -2,7 +2,7 @@ import boseapi.all as boseapi
 
 from xml.etree.ElementTree import Element
 
-device = boseapi.new_device('192.168.189.32')
+device = boseapi.new_device('127.0.0.1')
 with boseapi.SoundTouchClient(device) as client:
   # mute the device
   client.mute()
